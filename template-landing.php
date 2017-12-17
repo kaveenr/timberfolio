d2686d08e652ba985e441a64ae27048bdfbe6208
Kaveen Rodrigo <u.k.k.rodrigo@gmail.com>
2017-12-18 01:07:33 +0530

    gallery ordering fix

--- template-landing.php
+++ template-landing.php
@@ -1,13 +1,10 @@
 <?php
 /**
- * Template Name: Gallery Template
- * Description: Show Gallery
+ * Template Name: landing Template
+ * Description: show landing page
  */
 
 $context = Timber::get_context();
 $post = new TimberPost();
 $context['post'] = $post;
-if(get_post_gallery()){
-    $context['gallery'] = get_post_gallery(get_the_ID(),false);
-}
 Timber::render( array( 'page-' . $post->post_name . '.twig', 'page-gallery.twig' ), $context );
